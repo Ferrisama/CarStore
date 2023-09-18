ffdd546f2c0cf56e13f9d80c41b44233bb6ad51d
Ferrisama <94129345+Ferrisama@users.noreply.github.com>
2023-09-18 17:16:25 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Footer = () => (
   <footer className="flex flex-col text-black-100  mt-5 border-t">
     <div className="flex justify-between items-center flex-wrap mt-2 border-t border-white sm:px-16 px-6 py-10">
-      <p>@2023 Cars. All rights reserved</p>
+      <p>@2023 CoursePlaza. All rights reserved</p>
 
       <div className="footer__copyrights-link">
         <Link href="/" className="text-gray-500">
